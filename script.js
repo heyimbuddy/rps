d26b749c61a8739447ad3ec6192997e041754cb9
Matt <collinsamatthew@gmail.com>
2022-05-18 15:55:50 -0400

    computer play

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("Test");
+function random() {
+    return Math.floor(Math.random() * 3) + 1;
+}
+
+function computerPlay() {
+    let randomNumber = random();
+    if (randomNumber == 1) {
+        return "rock";
+    } else if (randomNumber == 2) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+console.log(computerPlay());
+console.log(computerPlay());
+console.log(computerPlay());
+console.log(computerPlay());
+console.log(computerPlay());
